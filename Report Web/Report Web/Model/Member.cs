68af75f205ad303df1e9a5194996727b1c8b66ef
nataehoon <skeoddla@gmail.com>
2023-01-22 15:06:33 +0900

    Login UI

---
+++ Report Web/Report Web/Model/Member.cs
@@ -0,0 +1,12 @@
+﻿namespace Report_Web.Model
+{
+    public class Member
+    {
+        [Display(Name = "아이디")]
+        public string? Id { get; set; }
+        [Display(Name = "비밀번호")]
+        public string? Pw { get; set; }
+        [Display(Name = "이메일")]
+        public string? Email { get; set; }
+    }
+}
