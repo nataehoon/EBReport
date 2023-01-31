dd0a53c69063d01b65d6198b6adbbab79003520d
nataehoon <skeoddla@gmail.com>
2023-01-31 11:20:14 +0900

    main CRUD

--- Report Web/Report Web/Data/ProjectManageService.cs
+++ Report Web/Report Web/Data/ProjectManageService.cs
@@ -15,6 +15,7 @@ namespace Report_Web.Data
             {
                 ProjectManage project = new ProjectManage();
 
+                project.Projectno = Convert.ToInt32(dt.Rows[i]["Projectno"]);
                 project.Title = dt.Rows[i]["Title"].ToString();
                 project.Manager = dt.Rows[i]["Managers"].ToString();
                 project.Startperiod = (DateTime)dt.Rows[i]["Startperiod"];
@@ -26,10 +27,27 @@ namespace Report_Web.Data
             return projectlist;
         }
 
-        public static void ProjectmanageCreate(ProjectManage model)
+        public static void ProjectManageCreate(ProjectManage model)
         {
-            string sql = $"INSERT INTO PROJECTMANAGE(Title, managers, Startperiod, Endperiod, Totalperiod, Progress) VALUES('{model.Title}','{model.Manager}', '{(DateTime)model.Startperiod}', '{(DateTime)model.Endperiod}', 0, 0)";
+            string Startperiod = model.Startperiod.ToString("yyyy/MM/dd");
+            string Endperiod = model.Endperiod.ToString("yyyy/MM/dd");
+            string sql = $"INSERT INTO PROJECTMANAGE(Title, Managers, Startperiod, Endperiod, Totalperiod, Progress) VALUES('{model.Title}','{model.Manager}', '{Startperiod}', '{Endperiod}', 0, 0)";
             SQLServer.SQLServerSelect(sql);
+        }
+
+        public static void ProjectManageUpdate(int no, ProjectManage model)
+        {
+            string Startperiod = model.Startperiod.ToString("yyyy/MM/dd");
+            string Endperiod = model.Endperiod.ToString("yyyy/MM/dd");
+            string sql = $"UPDATE PROJECTMANAGE SET Title='{model.Title}', Managers='{model.Manager}', Startperiod='{Startperiod}', Endperiod='{Endperiod}', Totalperiod='{model.Totalperiod}', Progress='{model.Progress}' WHERE Projectno = '{no}'";
+            SQLServer.SQLServerUpdate(sql);
+        }
+
+        public static void ProjectManageDelete(int no)
+        {
+            Console.WriteLine(no);
+            string sql = $"DELETE FROM PROJECTMANAGE WHERE Projectno = '{no}'";
+            SQLServer.SQLServerDelete(sql);
         }
     }
 }
